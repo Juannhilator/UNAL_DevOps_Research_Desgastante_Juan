0727de5dee0256d47c01cc946c7c16effb090277
Juan_Salazar <juancamilo785@gmail.com>
2024-11-02 12:33:45 -0500

    main tf created

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1"
+    }
+  }
+
+  required_version = ">= 0.12"
+}
+
+resource "null_resource" "fibonacci" {
+    triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "bash fibonacci.py"
+  }
+}
